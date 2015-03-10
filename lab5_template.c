80fca0e817d55a7e41e752825daf8a68ab53037e
grc <grc@Macbook.local>
2015-03-09 22:07:16 -0700

    Word frequency counter done

--- lab5_template.c
+++ lab5_template.c
@@ -37,6 +37,10 @@ bool print_word_frequencies(char* path);
    occurrences of strings. */
 struct WordCounts {
   /* TODO: add fields */
+  int maxCount;
+  int count;
+  int *keys;
+  char **words;
 };
 
 /* Allocate a new word count object, or return NULL on memory
@@ -62,6 +66,7 @@ void word_counts_print(struct WordCounts* wc);
 int main(int argc, char** argv) {
   if (argc != 2) {
     fputs("error: you must supply a filename as an argument\n", stderr);
+    fputs("USAGE: ./a.out <filePath>\n", stderr);
     return 1;
   }
 
@@ -77,7 +82,6 @@ bool print_word_frequencies(char* path) {
   FILE* f;
   struct WordCounts* wc;
   char str[MAX_WORD_LENGTH + 1];
-
   f = fopen(path, "r");
   if (f == NULL) {
     fprintf(stderr, "error: could not open '%s'", path);
@@ -91,15 +95,25 @@ bool print_word_frequencies(char* path) {
     return false;
   }
 
-  fprintf(stderr, "processing '%s'...", path);
+  fprintf(stderr, "processing '%s'...\n", path);
 
-  while ( /* TODO: read one word from f into str using fscanf */ ) {
+  char endFile;
+  int num = 0;
+
+  /* TODO: read one word from f into str using fscanf */
+  while (  fscanf(f,"%s",str) != EOF ) {
+ 
+    //printf("%s\n",str);
+    if(wc->maxCount < num){
+      wc->maxCount += 10; 
+    }
     if (!word_counts_increment(wc, str)) {
       fprintf(stderr, "\nerror: out of memory");
       fclose(f);
       word_counts_free(wc);
       return false;
     }
+    num++;
   }
 
   fclose(f);
@@ -120,21 +134,93 @@ struct WordCounts* word_counts_make() {
 
   /* TODO: initialize all fields */
 
+  it->maxCount = MAX_WORD_LENGTH;
+  it->count = 0;
+  it->keys = calloc(it->maxCount, sizeof(int));
+  it->words = (char **)malloc(it->maxCount * sizeof(char *));
+
+  for(int i = 0; i < it->maxCount; i++){
+    *(it->keys + i) = 0;
+    *(it->words + i) = "[   ]";
+    
+  }
+  
   return it;
 }
 
 void word_counts_free(struct WordCounts* wc) {
+
+  free(wc);
+  
   /* TODO */
 }
 
 int word_counts_lookup(struct WordCounts* wc, char* word) {
+   //printf("%d\n", wc->count);
+  
+  int returnValue = -2;
+
+  for(int i = 0; i < wc->count; i++){
+      
+      //printf("%d :: %s :: %s!!!\n",i, *(wc->words+i), word);
+      if(strcmp(*(wc->words+i),word) == 0){
+      returnValue = *(wc->keys+i);
+       // printf("BREAK!\n");
+        break;
+      }     
+    }
+
+  if(returnValue < 0){
+    returnValue = NOT_FOUND;
+   // printf("Not found!\n");
+  }
+
+  //printf("done!\n");
+  return returnValue;
   /* TODO */
 }
 
 bool word_counts_increment(struct WordCounts* wc, char* word) {
+  
+  bool returnValue = false;
+
+    int freq =  word_counts_lookup(wc, word);
+   // printf("%d\n", pos);
+    if(freq == -1){
+      char* newWord = strdup(word);
+      *(wc->words +  wc->count) = newWord;
+      *(wc->keys +  wc->count)  += 1;
+      wc->count += 1;
+      //printf("DIFF!\n\n");
+        returnValue = true;
+    }
+    else if(freq != -1){
+      for(int i = 0; i < wc->count; i++){
+      //printf("%d :: %s :: %s!!!\n",i, *(wc->words+i), word);
+        if(strcmp(*(wc->words+i),word) == 0){
+          *(wc->keys+ i) += 1;
+          //returnValue = true;
+       // printf("BREAK!\n");
+        break;
+      }     
+    }
+      //*(wc->keys+ pos) += 1;
+      returnValue = true;
+     //printf("SAME!\n\n");
+    }
+
+    //returnValue = true;
+
+  
+ 
+  return returnValue;
   /* TODO */
 }
 
 void word_counts_print(struct WordCounts* wc) {
   /* TODO */
+  
+  for(int i = 0; i < wc->count; i++){
+     printf("%s %d\n", *(wc->words+i), *(wc->keys+i));
+  }
 }
